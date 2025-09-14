68ec97476812e018cdc1321a509359493b3cfdf2
黃柊森 <joseph@huangzhongsens-MacBook-Air.local>
2025-09-14 17:14:00 +0800

    Modfiy Background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,36 +8,18 @@ import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      {/* <HomepageHeader /> */}
       <main>
-        <HomepageFeatures />
+        <div className="container">
+        <img src="/img/background.jpg" alt="Description of the image"/>
+          <h1 className="main-heading">Hello World, my name is Joseph.</h1>
+        </div>
       </main>
     </Layout>
   );
