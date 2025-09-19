696ad9871becf41d936f27d0e5472b906652921e
黃柊森 <joseph@huangzhongsens-MacBook-Air.local>
2025-09-19 22:14:13 +0800

    Add deploy path

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,14 +17,14 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Joseph.github.io/',
-
+  baseUrl: '/',
+  deploymentBranch: 'gh-pages',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Joseph.Huang', // Usually your GitHub org/user name.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
